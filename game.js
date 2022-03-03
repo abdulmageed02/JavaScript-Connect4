6b4c48f5ffe12f19f700b53a2128f92f73315415
abdulmageed02 <hossamabdulmageed@gmail.com>
2022-03-04 01:32:04 +0200

    testing referrer

--- game.js
+++ game.js
@@ -1,9 +1,16 @@
-// need to handle access to this page only from index.html
-let username = localStorage.getItem("username");
-let gameMode = localStorage.getItem("gameMode");
-let gameLevel = localStorage.getItem("gameLevel");
 
-console.log(username,gameMode,gameLevel);
+
+if( document.referrer != 'http://127.0.0.1:5500/index.html') {
+     alert("Page is Not accesissble this way");
+        document.location.assign("http://127.0.0.1:5500/index.html")
+}else{
+    let username = localStorage.getItem("username");
+    let gameMode = localStorage.getItem("gameMode");
+    let gameLevel = localStorage.getItem("gameLevel");
+    
+    console.log(username,gameMode,gameLevel);
+}
+
 
 //design el game 
         // based on game level game mode 
