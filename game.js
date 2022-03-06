d1a19b60aa59a30ae6b1bcba6a5518512c35d39a
sera 364 <galalsara0@gmail.com>
2022-03-06 17:14:17 +0200

    easy mode check win added

--- game.js
+++ game.js
@@ -71,3 +71,27 @@ function drawCircle(){
         // checkDiagonalClockWise();
         // checkDiagonalAntiClockWise();
 }}
+function checkHz(){
+        for(let r=0;r<rows;r++){
+                for(let c=0;c<columns-3;c++){
+                        if(board[r][c] != ' '){
+                        if(board[r][c]==board[r][c+1] && board[r][c+1]==board[r][c+2] && board[r][c+2]==board[r][c+3]){
+                                showWinnerStatus(board[r][c]);
+                                return;
+                        }
+                    }
+                }
+        }
+}
+function checkVl(){
+        for(let c=0;c<columns;c++){
+                for(let r=0;r<rows-3;r++){
+                        if(board[r][c] != ' '){
+                        if(board[r][c]==board[r+1][c] && board[r+1][c]==board[r+2][c] && board[r+2][c]==board[r+3][c]){
+                                showWinnerStatus(board[r][c]);
+                                return;
+                        }
+                    }
+                }
+        }
+}
