b4883588f5ea70501af62ea01e96d080e0af5686
muhammedgamal760 <muhammedgamal760@gmail.com>
2022-03-06 02:03:59 +0200

    setting game

--- game.js
+++ game.js
@@ -1,26 +1,73 @@
 
 
-if( document.referrer != 'http://127.0.0.1:5500/index.html') {
-     alert("Page is Not accesissble this way");
-        document.location.assign("http://127.0.0.1:5500/index.html")
-}else{
-    let username = localStorage.getItem("username");
-    let gameMode = localStorage.getItem("gameMode");
-    let gameLevel = localStorage.getItem("gameLevel");
+// if( document.referrer != 'http://127.0.0.1:5500/index.html') {
+//      alert("Page is Not accesissble this way!");
+//         document.location.assign("http://127.0.0.1:5500/index.html")
+// }else{
+//     let username = localStorage.getItem("username");
+//     let gameMode = localStorage.getItem("gameMode");
+//     let gameLevel = localStorage.getItem("gameLevel");
     
-    console.log(username,gameMode,gameLevel);
+//     console.log(username,gameMode,gameLevel);
+// }
+
+let playerRed = "Red"
+let playerYellow = "yellow"
+let currentPlayer = playerRed
+let gameOver = false
+let board;
+let rows = 6
+let columns = 7
+let freeColumns = []
+let pressed = false
+let startBtn = document.getElementById("btn");
+let circle;
+
+window.onload=function(){
+        setGame();
 }
 
-
-//design el game 
-        // based on game level game mode 
-        // show user name   
-
-// let start = document.getElementById("btn");
-// btn.addEventListener("click",draw);
-
-// function draw(){
+function setGame(){
+        board = []
+        freeColumns=[5,5,5,5,5,5,5]
+        for(r=0;r<rows;r++){
+                let row = []
+                for(c=0;c<columns;c++){
+                        row.push(" ");
+                        circle = document.createElement("div");
+                        circle.id=r.toString()+ "-" +c.toString();
+                        circle.classList.add("circle");
+                        document.getElementById("board").append(circle);
+                        circle.addEventListener("click",drawCircle);
+                }
+                board.push(row)
+        }
+}
+function drawCircle(){
+        if(pressed){
+        if(gameOver){
+                return;
+        }
+        let coord = this.id.split("-");
+        let r = Number(coord[0]);
+        let c = Number(coord[1]);
+        r = freeColumns[c];
+        if(r<0){return;}
+        board[r][c]=currentPlayer;
+        let currentCircle = document.getElementById(r.toString()+"-"+c.toString());
+        if(currentPlayer==playerRed){
+                currentCircle.classList.add("redCircle");
+                currentPlayer=playerYellow;
+        }
+        else{
+                currentCircle.classList.add("yellowCircle");
+                currentPlayer=playerRed;
+        }
+        console.log(r,c);
+        freeColumns[c]-=1;
         
-       
-//    document.getElementById("container").appendChild( document.createElement("div").document.className= "circle");
-// }
+        // checkHz();
+        // checkVl();
+        // checkDiagonalClockWise();
+        // checkDiagonalAntiClockWise();
+}}
