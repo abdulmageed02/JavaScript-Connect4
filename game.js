1c18ec22a47cce0b622a0bd5e5afee14d36e4070
sera 364 <galalsara0@gmail.com>
2022-03-08 01:39:49 +0200

    hard mode check win bug fix

--- game.js
+++ game.js
@@ -103,7 +103,7 @@ startBtn.addEventListener("click", () => {
 })
 function checkHzHARD(){
         for(let r=0;r<rows;r++){
-                for(let c=0;c<columns-3;c++){
+                for(let c=0;c<columns-4;c++){
                         if(board[r][c] != ' '){
                         if(board[r][c]==board[r][c+1] && board[r][c+1]==board[r][c+2] && board[r][c+2]==board[r][c+3] && board[r][c+3]==board[r][c+4] ){
                                 // winFlag=1;
@@ -116,7 +116,7 @@ function checkHzHARD(){
 }
 function checkVlHARD(){
         for(let c=0;c<columns;c++){
-                for(let r=0;r<rows-3;r++){
+                for(let r=0;r<rows-4;r++){
                         if(board[r][c] != ' '){
                         if(board[r][c]==board[r+1][c] && board[r+1][c]==board[r+2][c] && board[r+2][c]==board[r+3][c] && board[r+3][c]==board[r+4][c]){
                                 // winFlag=1;
